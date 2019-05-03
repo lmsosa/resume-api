d498f1f82fc4f2e2d88312d12ce58299f1bd2596
Luis Sosa <lsosa@makingsense.com>
2019-05-03 13:13:28 -0300

    Initial

---
+++ Resume.WebApi/Controllers/ImportacionController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Api.Controllers
+{
+    /// <summary>
+    /// Controller de Curriculums
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportacionController : ControllerBase
+    {
+        /// <summary>
+        /// Importar un archivo de curriculums
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <returns></returns>
+        [HttpPost("curriculums")]
+
+        public async Task<ActionResult<string>> ImportarCurriculums(IFormFile archivo)
+        {
+            // Copiar el archivo a una ubicación temporal.
+            var filePath = Path.GetTempFileName();
+
+            if (archivo.Length > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await archivo.CopyToAsync(stream);
+                }
+            }
+
+            // Procesar el archivo
+            
+
+            return Ok("Archivo procesado correctamente");
+        }
+
+    }
+}
